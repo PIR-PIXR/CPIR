eab2f38f4a223b4223f0325abc1e271448f08200
QUANG <caoquang91@gmail.com>
2023-02-07 13:33:42 +1100

    Update WY.c

--- WY.c
+++ WY.c
@@ -1,8 +1,14 @@
 #include <gmp.h>
+#include <time.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "blst/include/blst.h"
+#include "LMC.h"
+#include "utils.h"
 
 #define SIZE 255 //Each element's size is 256 bits
+#define DIGITS 77 //Number of digits in an element
 
 int position = 0; //position of E mapping function from 0 to n_value
 
@@ -252,4 +258,163 @@ mpz_t *Re_der_f(int server_ID, int l_pos, int t_private, int k_server, int m_val
 	}
 
 	return 	der_f_l;
+}
+
+
+//coefficients of polynomials in Server i for LMC
+void coefficients(mpz_t coef, int n_value, int** E_val, int k, int m_mapping, mpz_t queries_Q[k][m_mapping], char tmp[DIGITS], mpz_t p, blst_scalar ***F, int i)
+{
+	for (int iii = 1; iii <= n_value; iii++)
+	{	
+		mpz_set_ui (coef, 1);
+		//coefficients of polynomials in Server i as for F_Q
+		for (int jjj = 0; jjj < m_mapping; jjj++)
+		{
+			//Multiplies all z_l at E(i)_l = 1
+			if (E_val[iii - 1][jjj] == 1)
+			{
+				mpz_mul (coef, coef, queries_Q[i][jjj]);
+			}
+		}
+			mpz_mod (coef, coef, p);
+			mpz_get_str(tmp, 10, coef);
+			byte *in = tmp;
+			blst_scalar_from_be_bytes(&F[i][0][iii], in, 32);
+						
+			//coefficients of polynomials in Server i as for derivative F/z at Q
+			for (int pos = 0; pos < m_mapping; pos ++)
+			{
+				mpz_set_ui (coef, 1);
+				if (E_val[iii - 1][pos] == 1)
+				{
+					for (int jjj = 0; jjj < m_mapping; jjj++)
+					{
+						//Multiplies all z_l at E(i)_l = 1
+						if (E_val[iii - 1][jjj] == 1 && jjj != pos)
+						{
+							mpz_mul (coef, coef, queries_Q[i][jjj]);
+						}
+					}
+
+				}
+				mpz_mod (coef, coef, p);
+				mpz_get_str(tmp, 10, coef);
+				in = tmp;
+				blst_scalar_from_be_bytes(&F[i][pos + 1][iii], in, 32);
+			}
+	}
+}
+
+//Server computes y_i = F_i.x and send proof_i to the Client
+double WitnessGen(int m_mapping, int k_server, int n_value, blst_scalar *x, blst_scalar ***F, blst_p1 *H, blst_p1 proof[k_server][m_mapping+1], blst_scalar y[k_server][m_mapping+1], int i, clock_t start, clock_t stop)
+{
+	double s_time = 0.0;
+	
+	for (int l = 0; l < (m_mapping + 1); l++)
+	{
+		blst_fr Fi_r, x_r, yi_r, tmpi_r, sumi_r;
+		blst_fr_from_scalar(&Fi_r, &F[i][l][1]);
+		blst_fr_from_scalar(&x_r, &x[1]);
+		blst_fr_mul(&yi_r, &Fi_r, &x_r);
+		    			
+		for (int o = 2; o <= n_value; o++)
+		{
+			blst_fr_from_scalar(&Fi_r, &F[i][l][o]);
+			blst_fr_from_scalar(&x_r, &x[o]);
+			blst_fr_mul(&tmpi_r, &Fi_r, &x_r);
+			blst_fr_add(&sumi_r, &yi_r, &tmpi_r);
+			yi_r = sumi_r;
+		}
+		blst_scalar_from_fr(&y[i][l], &yi_r);
+		    				
+		//4.3. Sends proof_i to the Client as for F_Q
+		start = clock();
+		open(n_value, x, F[i][l], H, &proof[i][l]);
+		stop = clock();
+		printf("LMC: Proof[Server_%d][%d] time = %lf seconds\n\n", i, l, (double) (stop - start) / CLOCKS_PER_SEC);
+			    			
+		s_time += (double) (stop - start) / CLOCKS_PER_SEC;
+	}
+	return s_time;
+}
+
+//Client starts to reconstruct x_i
+void Reconstruct_Client(int degree_d, int k_server, int t_private, int m_value, mpz_t lambda_random[k_server][t_private], mpz_t result[m_value + 1], mpz_t p, mpz_t** F_Q, mpz_t** derf_lambda)
+{
+	mpz_t* det;
+	mpz_t* det_a;
+	mpz_t rop;
+	mpz_init (rop);
+	mpz_t matrix[degree_d + 1][degree_d + 1];
+		    		
+	for (int i = 0; i < degree_d + 1; i++)
+	{
+		for (int j = 0; j < degree_d + 1; j++)
+		{
+		    	mpz_init (matrix[i][j]);
+		}
+	}
+		    		
+	//Creates Matrix D
+	int id = 0; //Server_ID
+	for (int ii = 0; ii < (degree_d + 1); ii++)
+	{
+		if (ii % 2 == 0)
+		{
+			mpz_set_ui(matrix[ii][0], 1);
+			mpz_set (matrix[ii][1], lambda_random[id][0]);
+			id++;
+		}
+		else
+		{
+			mpz_set_ui(matrix[ii][0], 0);
+			mpz_set_ui(matrix[ii][1], 1);
+		}
+			   		
+		for (int jj = 2; jj < (degree_d + 1); jj++)
+		{
+			if (ii % 2 == 0)
+			{
+				mpz_mul (matrix[ii][jj], matrix[ii][1], matrix[ii][jj - 1]);
+				mpz_mod (matrix[ii][jj], matrix[ii][jj], p);
+			}
+			else
+			{
+			   	mpz_mul_ui (matrix[ii][jj], matrix[ii - 1][jj - 1], jj);
+			   	mpz_mod (matrix[ii][jj], matrix[ii][jj], p);
+			}
+		}
+	}
+	//det(D)
+	det = Determinant((degree_d + 1), matrix, (degree_d + 1));
+	mpz_invert (det[0], det[0], p);
+			   	
+	//Solving the linear equations for (d + 1) variables
+	id = 0;
+	for (int j = 0; j < (m_value + 1); j++)
+	{
+		for (int i = 0; i < k_server; i++)
+		{
+			if (id < (degree_d + 1))
+			{
+				mpz_set (matrix[id][0], F_Q[i][j]);
+				id++;
+				if (id < (degree_d + 1))
+				{
+				   	mpz_set (matrix[id][0], derf_lambda[i][j]);
+				   	id++;
+				}
+				else i = k_server;
+			}
+			else i = k_server;	   			
+		}
+		id = 0;
+					
+		det_a = Determinant((degree_d + 1), matrix, (degree_d + 1));
+		mpz_mul (rop, det_a[0], det[0]);
+		mpz_mod (rop, rop, p);
+		mpz_init_set(result[j], rop);
+		mpz_clear(det_a[0]);
+		free(det_a);
+	}
 }
