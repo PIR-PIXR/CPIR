d25d8894181192e631681065b3370adc064d20bd
QUANG <caoquang91@gmail.com>
2023-02-08 10:41:19 +1100

    Update CKGS.h

--- CKGS.h
+++ CKGS.h
@@ -1,10 +1,10 @@
 // CKGS.h
 #pragma once
 
-//Clients creates random queries (binary vectors size n) (position = i_index)
+//Client creates random queries (binary vectors size n) (position = i_index)
 int** QueriesGen_Client (int position, int n);
 
-//The server will respond the anwser when received query
+//The Server will respond the anwsers when received query
 mpz_t** AnwGen_Server (int m, int n, int** queries, mpz_t *X_db,  mpz_t *hash_cols, mpz_t p);
 
 //Server computes y_i = F_i.x and send proof_i to the Client
