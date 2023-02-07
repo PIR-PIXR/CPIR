ee54ef8509ff0874f343bcaff52a4ad2e7b16ffc
QUANG <caoquang91@gmail.com>
2023-02-08 10:32:39 +1100

    Update GenCKGS.c

--- GenCKGS.c
+++ GenCKGS.c
@@ -1,11 +1,15 @@
 #include <gmp.h>
+#include <time.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include "blst/include/blst.h"
+#include "LMC.h"
 #include "utils.h"
 
 #define SIZE 255 //Each element's size is 256 bits
 #define DIGITS 77 //Number of digits in an element
 
-//Clients creates random queries (binary vectors size n) (position = i_index)
+//Client creates random queries (binary vectors size n) (position = i_index)
 mpz_t** QueriesGen_Client (int position, int n, int k, mpz_t p)
 {
 	mpz_t tmp;
@@ -100,4 +104,43 @@ void Extract_Client (int m, int k, mpz_t p, mpz_t** answers, mpz_t result[m + 1]
 	{
 		mpz_mod (result[i], result[i], p);
 	}
+}
+
+//Server computes y_i = F_i.x and send proof_i to the Client
+double WitnessGen(int n_value, int k_server, mpz_t** queries, blst_scalar *x, blst_scalar **F, blst_p1 *H, blst_p1 proof[k_server], blst_scalar y[k_server], int i, clock_t start, clock_t stop)
+{
+	char tmp[DIGITS];
+	double s_time = 0.0;
+	
+	for (int jj = 1; jj <= n_value; jj++)
+	{
+		mpz_get_str(tmp, 10, queries[i][jj - 1]);
+		byte *in = tmp;
+		blst_scalar_from_be_bytes(&F[i][jj], in, 32);
+	}
+					
+	//Computes y_i = F_i.x and send to the Client
+	blst_fr Fi_r, x_r, yi_r, tmpi_r, sumi_r;
+	blst_fr_from_scalar(&Fi_r, &F[i][1]);
+	blst_fr_from_scalar(&x_r, &x[1]);
+	blst_fr_mul(&yi_r, &Fi_r, &x_r);
+					
+	for (int o = 2; o <= n_value; o++)
+	{
+		blst_fr_from_scalar(&Fi_r, &F[i][o]);
+		blst_fr_from_scalar(&x_r, &x[o]);
+		blst_fr_mul(&tmpi_r, &Fi_r, &x_r);
+		blst_fr_add(&sumi_r, &yi_r, &tmpi_r);
+		yi_r = sumi_r;
+	}
+	blst_scalar_from_fr(&y[i], &yi_r);
+		    			
+	//Sends proof_i to the Client
+	start= clock();
+	open(n_value, x, F[i], H, &proof[i]);
+	stop = clock();   		
+	s_time = (double) (stop - start) / CLOCKS_PER_SEC;
+	printf("\nLMC: Proof_%d time = %lf seconds", i, s_time);
+	
+	return s_time;
 }
