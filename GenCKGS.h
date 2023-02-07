918914a2d9d73b70c1aba40ea0432b53d6523801
QUANG <caoquang91@gmail.com>
2023-02-08 10:33:13 +1100

    Update GenCKGS.h

--- GenCKGS.h
+++ GenCKGS.h
@@ -1,7 +1,7 @@
 // GenCKGS.h
 #pragma once
 
-//Clients creates random queries (binary vectors size n) (position = i_index)
+//Client creates random queries (binary vectors size n) (position = i_index)
 mpz_t** QueriesGen_Client (int position, int n, int k, mpz_t p);
 
 //The server will respond the anwser when received query
@@ -9,3 +9,6 @@ mpz_t** AnwGen_Server (int m, int n, mpz_t** queries, mpz_t** DataBase_T, int k,
 
 //After received all anwsers from two servers, the client will decode to get x_i and h(x_i) values
 void Extract_Client (int m, int k, mpz_t p, mpz_t** answers, mpz_t result[m + 1]);
+
+//Server computes y_i = F_i.x and send proof_i to the Client
+double WitnessGen(int n_value, int k_server, mpz_t** queries, blst_scalar *x, blst_scalar **F, blst_p1 *H, blst_p1 proof[k_server], blst_scalar y[k_server], int i, clock_t start, clock_t stop);
